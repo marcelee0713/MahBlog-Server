436279f0f58e140b6ceacfb44d2eb49ef6f42447
Marcel Magbual <magbualmarcel@gmail.com>
2024-12-11 23:15:08 +0800

    refactor:  maximum limit for json and urlencoded data

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,8 @@ dotenv.config();
 export const app = express();
 
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
 app.use(
   cors({
